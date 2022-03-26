9ab9aa78334f71a5a04049b28ead7853b5ca8529
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-26 14:17:53 +0530

    Refactored initializing deck of cards  method

--- src/com/blz/cards/DeckOfCards.java
+++ src/com/blz/cards/DeckOfCards.java
@@ -1,16 +1,17 @@
 package com.blz.cards;
 
-public class DeckOfCards{
-
-    private static final String[] SUITS = { "CLUBS", "DIAMOND", "HEARTS", "SPADES" };
-    private static final String[] RANKS = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
-    private final int SIZE_OF_CARDS= SUITS.length * RANKS.length;
+public class DeckOfCards implements IDeckOfCards{
     public String[] deck = new String[SIZE_OF_CARDS];
-
 
     public void initializing() {
         for (int i = 0; i < deck.length; i++) {
-            deck[i] = RANKS[i % 13] + " Of "+ SUITS[i / 13];
+
+            String unique = RANKS[i % 13] + " Of "+ SUITS[i / 13];
+            if(deck[i] == null){
+                deck[i] = unique;
+            } else  {
+
+            }
         }
     }
 
--- src/com/blz/cards/IDeckOfCards.java
+++ src/com/blz/cards/IDeckOfCards.java
@@ -1,13 +1,11 @@
 package com.blz.cards;
 
 public interface IDeckOfCards {
-//    private String[] SUITS   = { "CLUBS", "DIAMOND", "HEARTS", "SPADES" };
-//    private String[] RANKS  = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
-//    private final int SIZE_OF_CARDS= SUITS.length * RANKS.length;
-//    public String[] deck = new String[SIZE_OF_CARDS];
+    public String[] SUITS   = { "CLUBS", "DIAMOND", "HEARTS", "SPADES" };
+    public String[] RANKS  = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+    public final int SIZE_OF_CARDS= SUITS.length * RANKS.length;
 
-    public void initializing(String[] suits, String[] ranks);
+    public void initializing();
     public void print();
 
-    void print(String[] deck);
 }
